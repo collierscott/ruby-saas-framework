2de14bafaa268fb582bc13ca2709a1b554dc146b
Scott A Collier <scott@onlinespaces.com>
2016-03-20 00:16:46 +0000

    @T34 Add flash notifications

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,9 +7,11 @@ class ContactsController < ApplicationController
         @contact = Contact.new(contact_params)
         
         if @contact.save
-            redirect_to new_contact_path, notice: "Message sent."
+            flash[:success] = 'Message sent.'
+            redirect_to new_contact_path
         else 
-            redirect_to new_contact_path, notice: "Error occurred."
+            flash[:danger] = 'Error occured, message has not been sent.'
+            redirect_to new_contact_path
         end
     end
     
