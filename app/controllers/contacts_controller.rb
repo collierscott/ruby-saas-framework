77c5d8b0a133644d790dd5ade17ecb0bc3af4bf8
Scott A Collier <scott@onlinespaces.com>
2016-03-20 01:02:41 +0000

    @T36 Fix formatting in contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,6 +10,7 @@ class ContactsController < ApplicationController
             name = params[:contact][:name]
             email = params[:contact][:email]
             body = params[:contact][:comments]
+            
             ContactMailer.contact_email(name, email, body).deliver
             flash[:success] = 'Message sent.'
             redirect_to new_contact_path
