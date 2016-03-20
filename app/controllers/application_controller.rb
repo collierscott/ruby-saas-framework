5521b4c3f7559286da79a5115253927f1e3d363a
Scott A Collier <scott@onlinespaces.com>
2016-03-20 15:52:07 +0000

    @T40 Modify devise for stripe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_filter :configure_premitted_parameters, if: :divise_controller?
+  
+  protected
+    def configure_premitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :stripe_card_token, :email, :password, :password_confirmation)
+    end
 end
