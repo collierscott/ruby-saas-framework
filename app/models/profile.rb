5581d1b0ef656d76b6b7a9cee3a3885314b29380
Scott A Collier <scott@onlinespaces.com>
2016-03-21 21:27:41 +0000

    Add avatar to model and db

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,4 +3,8 @@ class Profile < ActiveRecord::Base
    validates :first_name, presence: true
    validates :last_name, presence: true
    validates :contact_email, presence: true
+   
+   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+      validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+   
 end
