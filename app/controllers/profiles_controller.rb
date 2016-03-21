56bb2511f1cb3f958fcf0b11642c75eec51a7f80
Scott A Collier <scott@onlinespaces.com>
2016-03-21 20:29:12 +0000

    Add the show action for the user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,4 +4,22 @@ class ProfilesController < ApplicationController
         @user = User.find( params[:user_id] )   #find who is logged in
         @profile = @user.build_profile          #build profile for user that is logged in
     end
+    
+    def create 
+        @user = User.find( params[:user_id] )
+        @profile = @user.build_profile(profile_params)
+        if @profile.save
+            flash[:success] = "Profile Updated!"
+            redirect_to user_path( params[:user_id] )
+        else
+            flash[:danger] = "No Way! Please shck the form."
+            render action: :new
+        end
+    end
+
+    private
+        def profile_params
+            # param whitelist
+            params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+        end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,6 @@
 class Profile < ActiveRecord::Base
    belongs_to :user 
+   validates :first_name, presence: true
+   validates :last_name, presence: true
+   validates :contact_email, presence: true
 end
