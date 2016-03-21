1a23c3ab3452b307053e960bc261f9452edc7427
Scott A Collier <scott@onlinespaces.com>
2016-03-21 20:45:13 +0000

    Fix issue where visiting create profile page vaused error

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
     def new
         # form where a user can fill out their own profile.
         @user = User.find( params[:user_id] )   #find who is logged in
-        @profile = @user.build_profile          #build profile for user that is logged in
+        @profile = Profile.new          #build profile for user that is logged in
     end
     
     def create 
