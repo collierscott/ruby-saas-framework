baf9e1bae3d4ab4069163c66a6333602d1dd08fa
Scott A Collier <scott@onlinespaces.com>
2016-03-21 19:44:06 +0000

    Add profile controller new action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,8 @@
 class ProfilesController < ApplicationController
-    
+    def new
+        # form where a user can fill out their own profile.
+        @user = User.find( params[:user_id] )   #find who is logged in
+        @variable = params[:hello]
+        @profile = @user.build_profile          #build profile for user that is logged in
+    end
 end
